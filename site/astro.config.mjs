89be1c7a882823bd8bdda5a8ab585031ed330e41
Nel Fernandes <46218259+imNel@users.noreply.github.com>
2023-12-12 22:35:49 +0800

    Add svg logo to site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,40 +1,42 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Sunscreen',
-			social: {
-				github: 'https://github.com/decompyl/sunscreen',
-			},
-			sidebar: [
-				{
-						label: 'Getting Started',
-						link: '/getting-started/',
-				},
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [
-						{
-							label: '@sunscreen/core',
-							autogenerate: { directory: 'reference/core' },
-						}
-
-					]
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [
+    starlight({
+      title: "Sunscreen",
+      logo: {
+        src: "./src/assets/sunscreen-logo.svg",
+      },
+      social: {
+        github: "https://github.com/decompyl/sunscreen",
+      },
+      sidebar: [
+        {
+          label: "Getting Started",
+          link: "/getting-started/",
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          items: [
+            {
+              label: "@sunscreen/core",
+              autogenerate: { directory: "reference/core" },
+            },
+          ],
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
