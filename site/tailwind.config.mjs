be0bc5e3bb37e113189deac25a604db95e47362f
Josh <66529773+Joshuahuahua@users.noreply.github.com>
2023-12-13 02:30:59 +0800

    Initial animation tests

--- site/tailwind.config.mjs
+++ site/tailwind.config.mjs
@@ -12,6 +12,31 @@ export default {
         // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
         gray: colors.neutral,
       },
+      animation: {
+        typewriter: 'typewriter 1s steps(5) forwards',
+        caret: 'typewriter 1s steps(5) forwards, blink 0.75s steps(10,start) 2',
+        rotateCw: '1.5s rotateCw 2s forwards',
+        rotateCcw: '1.5s rotateCcw 2s forwards',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          'from, to': { opacity: '0' },
+          '50%': { opacity: '1' },
+        },
+        rotateCw: {
+          'from': { transform: 'rotate(0deg)' },
+          'to': { transform: 'rotate(45deg)' },
+        },
+        rotateCcw: {
+          'from': { transform: 'rotate(0deg)' },
+          'to': { transform: 'rotate(-45deg)' },
+        },
+      }
     },
   },
   plugins: [starlightPlugin()],
